fca9dbc157da4dfbf2f6de82f16babb035195bd6
Patrick da Silva Almeida <patrick.almeida06@gmail.com>
2025-07-28 21:10:21 -0300

    fix:attemp zero

--- controllers/agentesController.js
+++ controllers/agentesController.js
@@ -71,7 +71,7 @@ function create(req, res, next) {
   try {
     const parsed = AgenteSchema.safeParse(req.body);
     if (!parsed.success) {
-      return res.status(404).json({ message: parsed.error.errors[0].message });
+      return res.status(400).json({ message: parsed.error.errors[0].message });
     }
 
     const NewAgente = { id: uuidv4(), ...req.body };
@@ -116,7 +116,7 @@ function updateAgente(req, res, next) {
     }
     const parsed = AgenteSchema.safeParse(req.body);
     if (!parsed.success) {
-      return res.status(404).json({ message: parsed.error.errors[0].message });
+      return res.status(400).json({ message: parsed.error.errors[0].message });
     }
     const agenteUpdated = agentesRepository.updateAgente(id, req.body);
     if (agenteUpdated === null) {
@@ -145,7 +145,7 @@ function patch(req, res, next) {
     }
     const parsed = AgentePartial.safeParse(req.body);
     if (!parsed.success) {
-      return res.status(404).json({ message: parsed.error.errors[0].message });
+      return res.status(400).json({ message: parsed.error.errors[0].message });
     }
     const agenteUpdated = agentesRepository.patch(id, req.body);
     if (agenteUpdated === null) {
